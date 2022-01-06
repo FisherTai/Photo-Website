115ca9e16c0cde395d15dc651341686f0dcf2349
LAPTOP-1SGCLO1T\Fisher <unifisher02@gmail.com>
2022-01-06 23:53:12 +0800

    Create

---
+++ src/pages/Homepage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Search from '../components/Search';
+
+
+const Homepage = () => {
+    const auth = "563492ad6f91700001000001647b4227b11544b1885d32353a50aba8";
+    return (
+        <div style={{minHeight : "100vh"}}>
+            <Search />
+        </div>
+    )
+}
+
+export default Homepage
