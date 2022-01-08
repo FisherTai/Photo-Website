362e491ab1cc5c6dd1aa8428ad53cc15a34d0b6a
LAPTOP-1SGCLO1T\Fisher <unifisher02@gmail.com>
2022-01-08 19:06:50 +0800

    增加"更多"按鈕

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -4,13 +4,17 @@ import Picture from "../components/Picture";
 
 const Homepage = () => {
   const [input, setInput] = useState("");
+  const [page, setPage] = useState(1);
   const searchUrl = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=1`;
   let [data, setData] = useState(null);
   const auth = "563492ad6f91700001000001647b4227b11544b1885d32353a50aba8";
-  const initUrl = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  const initUrl = `https://api.pexels.com/v1/curated?page=1&per_page=15`;
 
   // fetch data from pexels api
   const search = async (url) => {
+    setPage(2);
+    console.log(url);
+
     const dataFetch = await fetch(url, {
       method: "GET",
       headers: {
@@ -22,6 +26,28 @@ const Homepage = () => {
     setData(parseData.photos);
   };
 
+  //獲取更多
+  const morepicture = async() =>{
+    let newURL;
+    if(input === ""){
+      newURL = `https://api.pexels.com/v1/curated?page=${page}&per_page=15`;
+    } else{
+      newURL = `https://api.pexels.com/v1/search?query=${input}&per_page=15&page=${page}`;
+    }
+    console.log(newURL);
+    const dataFetch = await fetch(newURL, {
+      method: "GET",
+      headers: {
+        Accept: "application/json",
+        Authorization: auth,
+      },
+    });
+    setPage(page+1); //這邊的參數是一個closure，下次進來後值才會被變更
+    let parseData = await dataFetch.json();
+    setData(data.concat(parseData.photos));
+  }
+
+
   // 進入頁面時執行
   useEffect(() => {
     search(initUrl);
@@ -29,12 +55,15 @@ const Homepage = () => {
 
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Search search={() => search(searchUrl)} setInput={setInput}/>
+      <Search search={() => search(searchUrl)} setInput={setInput} />
       <div className="pictures">
         {data &&
           data.map((d) => {
             return <Picture data={d} />;
           })}
+      </div>
+      <div className="morePictures">
+        <button onClick={morepicture}>Load More</button>
       </div>
     </div>
   );
